31ab88e9755073e8feddf530b1de92599b19be0c
kapil619 <kapilbadokar321@gmail.com>
2023-11-22 18:19:00 +0530

    Created ViewImage Screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, StatusBar, Alert, Platform } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 // import { useDimensions, useDeviceOrientation } from 'react-native-community/hooks';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
+
   );
 }
 
