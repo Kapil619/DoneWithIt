f07ed404a49a95f71078a5e5022f777dd2ac3c66
kapil619 <kapilbadokar321@gmail.com>
2023-11-24 15:55:12 +0530

    Created a icon component

--- App.js
+++ App.js
@@ -5,13 +5,22 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
-
+import Screen from './app/components/Screen';
+import Icon from './app/components/icon';
 // import { useDimensions, useDeviceOrientation } from 'react-native-community/hooks';
 
 export default function App() {
   return (
 
-    <MessagesScreen />
+    <Screen>
+      <Icon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white"
+      />
+
+    </Screen>
   );
 }
 
