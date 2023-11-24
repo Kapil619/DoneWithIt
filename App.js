aa8fe9d84224b7599cc33b674b85607857cf7db4
kapil619 <kapilbadokar321@gmail.com>
2023-11-24 12:10:02 +0530

    Updated Icons for View ImageScreen

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {
   return (
 
-    <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
 
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,17 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import colors from '../config/colors';
-
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-            <View style={styles.closeIcon}></View>
-            <View style={styles.deleteIcon}></View>
+            <View style={styles.closeIcon}>
+
+                <MaterialCommunityIcons name='close' color='white' size={30} />
+            </View>
+            <View style={styles.deleteIcon}>
+                <MaterialCommunityIcons name='trash-can-outline' color='white' size={30} />
+            </View>
             <Image resizeMode='contain' style={styles.image} source={require('../assets/chair.jpg')} />
         </View>
     );
@@ -19,22 +24,14 @@ const styles = StyleSheet.create({
 
     },
     closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
         position: 'absolute',
         top: 40,
         left: 30,
-
     },
     deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.secondary,
         position: 'absolute',
         top: 40,
         right: 30,
-
     },
     image: {
         width: '100%',
