458eaf5cb83810f5ae16381170e74e9401aff778
kapil619 <kapilbadokar321@gmail.com>
2023-11-25 23:43:19 +0530

    Refactored styles into a new styles config file

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
+import defaultStyles from '../config/styles';
 
 function AppText({ children, style }) {
     return (
-        <Text style={[styles.text, style]}>{children}</Text>
+        <Text style={[defaultStyles.text, style]}>{children}</Text>
     );
 }
 
-const styles = StyleSheet.create({
-    text: {
-        fontSize: 18,
-        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    }
-})
 
 export default AppText;
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,31 +1,25 @@
-import React from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import colors from '../config/colors';
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import defaultStyles from '../config/styles';
 
 function AppTextInput({ icon, ...otherProps }) {
     return (
         <View style={styles.container}>
-            {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon} />}
-            <TextInput style={styles.textInput} {...otherProps} />
+            {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
+            <TextInput style={defaultStyles.text} {...otherProps} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: colors.light,
+        backgroundColor: defaultStyles.colors.light,
         borderRadius: 25,
         flexDirection: 'row',
         width: '100%',
         padding: 15,
         marginVertical: 10,
-    },
-
-    textInput: {
-        color: colors.dark,
-        fontSize: 18,
-        fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
     },
     icon: {
         marginRight: 10,
