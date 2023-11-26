64e0563f96375dd66a76a0b79f51579845068439
kapil619 <kapilbadokar321@gmail.com>
2023-11-26 12:04:34 +0530

    Fixed Picker & Modal issue. Show picker items when modal is clicked. Created new PickerItem comp.

--- App.js
+++ App.js
@@ -11,14 +11,23 @@ import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen';
 import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+import { useState } from 'react';
 // import { useDimensions, useDeviceOrientation } from 'react-native-community/hooks';
 
+
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Technology', value: 3 },
+]
 export default function App() {
+  const [category, setcategory] = useState(categories[0])
   return (
 
     <Screen>
-      <AppPicker icon={'apps'} placeholder="Category" />
+      <AppPicker selectedItem={category} onSelectItem={item => setcategory(item)} items={categories} icon={'apps'} placeholder={'Category'} />
       <AppTextInput icon={'email'} placeholder="Email" />
     </Screen>
   );
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,39 +1,43 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { Button, Modal, StyleSheet, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+import { Button, FlatList, Modal, StyleSheet, TextInput, TouchableWithoutFeedback, View } from 'react-native';
 import defaultStyles from '../config/styles';
 import AppText from './AppText';
 import Screen from './Screen';
+import PickerItem from './PickerItem';
 
-
-function AppTextInput({ icon, placeholder, ...otherProps }) {
+function AppPicker({ icon, items, placeholder, selectedItem, onSelectItem }) {
     const [modalVisible, setmodalVisible] = useState(false)
 
     return (
         <>
+
             <TouchableWithoutFeedback onPress={() => setmodalVisible(true)}>
                 <View style={styles.container}>
                     {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
-                    <AppText style={styles.text}> {placeholder} </AppText>
+                    <AppText style={styles.text}> {selectedItem ? selectedItem.label : placeholder} </AppText>
                     <MaterialCommunityIcons
                         name='chevron-down'
                         size={20}
-                        color={defaultStyles.colors.black} />
-
+                        color={defaultStyles.colors.medium}
+                    />
                 </View>
             </TouchableWithoutFeedback>
             <Modal visible={modalVisible} animationType='slide' >
                 <Screen>
                     <Button title='Close' onPress={() => setmodalVisible(false)} />
-
+                    <FlatList data={items} keyExtractor={item => item.value.toString()}
+                        renderItem={({ item }) => <PickerItem label={item.label}
+                            onPress={() => {
+                                setmodalVisible(false);
+                                onSelectItem(item);
+                            }} />
+                        } />
                 </Screen>
             </Modal>
         </>
-
-
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         backgroundColor: defaultStyles.colors.light,
@@ -50,5 +54,4 @@ const styles = StyleSheet.create({
         flex: 1,
     }
 })
-
-export default AppTextInput;
+export default AppPicker;
