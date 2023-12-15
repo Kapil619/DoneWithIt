221982e48b243935752a6c7e18f7b98086baf45e
kapil619 <kapilbadokar321@gmail.com>
2023-12-15 18:12:14 +0530

    Added horizonatal scrolling to the component

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { ScrollView, View } from 'react-native';
 import { StyleSheet } from 'react-native';
 import ImageInput from './ImageInput';
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
-    return (
-        <View style={styles.container}>
-            {imageUris.map(uri => (
-                <View key={uri} style={styles.image}>
-                    <ImageInput imageUri={uri}
-                        onChangeImage={() => onRemoveImage(uri)} />
-                </View>
-            ))}
 
-            <ImageInput onChangeImage={uri => onAddImage(uri)} />
+    const scrollView = useRef();
+
+    return (
+        <View>
+            <ScrollView ref={scrollView} horizontal onContentSizeChange={() => scrollView.current.scrollToEnd()} >
+                <View style={styles.container}>
+                    {imageUris.map(uri => (
+                        <View key={uri} style={styles.image}>
+                            <ImageInput imageUri={uri}
+                                onChangeImage={() => onRemoveImage(uri)} />
+                        </View>
+                    ))}
+                    <ImageInput onChangeImage={uri => onAddImage(uri)} />
+                </View>
+            </ScrollView>
         </View>
     );
 }
-
-
-
 const styles = StyleSheet.create({
 
     container: {
