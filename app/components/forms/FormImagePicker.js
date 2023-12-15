a0726bfc1a0225e984f0f21df32ca721be0e5282
kapil619 <kapilbadokar321@gmail.com>
2023-12-15 18:20:02 +0530

    Built a new component for image picking on forms

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ImageInputList from '../ImageInputList';
+import ErrorMessage from './ErrorMessage';
+import { useFormikContext } from 'formik';
+
+function FormImagePicker({ name }) {
+    const { errors, setFieldValue, touched, values } = useFormikContext();
+    const imageUris = values[name];
+    const handleAdd = uri => {
+        setFieldValue(name, [imageUris, uri]);
+    };
+
+    const handleRemove = uri => {
+        setimageUris(name, imageUris.filter(imageUri => imageUri !== uri))
+    };
+
+    return (
+        <>
+            <ImageInputList imageUris={imageUris}
+                onAddImage={handleAdd}
+                onRemoveImage={handleRemove} />
+            <ErrorMessage error={errors[name]} visible={touched[name]} />
+        </>
+
+
+    );
+}
+
+export default FormImagePicker;
